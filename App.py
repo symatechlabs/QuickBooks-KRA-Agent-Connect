6a94a9139e3b97578c5220fdcbd1e88537c8aa36
Symatech Labs Ltd <32832450+symatechlabs@users.noreply.github.com>
2025-05-21 21:46:16 +0300

    Update App.py

--- App.py
+++ App.py
@@ -7,5 +7,5 @@ sales = Sales();
 
 #print(qConnet.getAuthCode());
 #print(qConnet.getTokens());
-#print(qConnet.refreshToken("RT1-38-H0-1756576215796cwos6969iy279scrw"));
+#print(qConnet.refreshToken(""));
 #print(sales.getSalesReceipts("","", ""))
